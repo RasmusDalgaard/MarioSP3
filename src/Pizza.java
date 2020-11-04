bc79ef6e8adb9ae64d964acdc0607c345c6cdfb2
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2020-11-04 12:29:54 +0100

    added resources, pizza class, and menu class

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,49 @@
 public class Pizza {
+    int id;
+    private final double price;
+    private final String title;
+    private final String topping;
+
+    public Pizza(int id, double price, String title, String topping) {
+        this.id = id;
+        this.price = price;
+        this.title = title;
+        this.topping = topping;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTopping() {
+        return topping;
+    }
+
+    private int getLength() {
+        String value = getId() + ". " + getTitle() + ": " + getTopping() + " " + getPrice() + ",-";
+        return value.length();
+    }
+
+    private String getSpace() {
+        int max = 92;
+        int totalDots = max - getLength();
+        String dots = "";
+        for (int i = 0; i < totalDots; i++) {
+            dots += ".";
+        }
+        return dots;
+    }
+
+    @Override
+    public String toString() {
+        return getId() + ". " + getTitle() + ": " + getTopping() + getSpace() + " " + getPrice() + ",-";
+    }
 }
